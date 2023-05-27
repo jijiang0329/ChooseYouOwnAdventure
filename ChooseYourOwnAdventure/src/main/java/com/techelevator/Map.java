1d982795d3f7660e888eb8354c093be31aafa492
Angel Acosta <angelofsd@gmail.com>
2023-05-27 10:02:16 -0600

    map update

--- ChooseYourOwnAdventure/src/main/java/com/techelevator/Map.java
+++ ChooseYourOwnAdventure/src/main/java/com/techelevator/Map.java
@@ -8,18 +8,18 @@ public class Map {
         System.out.println("║                                                                        ║");
         System.out.println("║                             ╔═════╗                                    ║");
         System.out.println("║                             ║ BLUE║                                    ║");
-        System.out.println("║                             ╚═════╝                                    ║");
-        System.out.println("║                               ▲                                        ║");
+        System.out.println("║                             ╚═▲═══╝                                    ║");
+        System.out.println("║                               │                                        ║");
         System.out.println("║                               │                                        ║");
         System.out.println("║  ╔═══════╗   ╗═════╔          │               ╔═══════╗                ║");
-        System.out.println("║  ║ PURPLE║◄──║ RED ║◄───═╔═══════╬═══════╗───►║ GREEN ║◄═─╗            ║");
-        System.out.println("║  ║═══════║   ╚═════╝     ║ START ║       ╚════╝═══════║   │            ║");
-        System.out.println("║                          ╚═══════╝                    │   │            ║");
-        System.out.println("║                              │                        │   ▼            ║");
-        System.out.println("║                              ▼                        │ ╔══════╗       ║");
-        System.out.println("║                         ╔══════╗                      ╢ ║YELLOW║       ║");
-        System.out.println("║                         ║VIOLET║                      ║ ╚══════╝       ║");
-        System.out.println("║                         ╚══════╝                      ║                ║");
+        System.out.println("║  ║ PURPLE║◄──║ RED ║◄───═╔═══════╬───────────►║ GREEN ║◄═─╗            ║");
+        System.out.println("║  ║═══════║   ╚═════╝     ║ START ║            ╝═══════║   │            ║");
+        System.out.println("║                          ╚═══════╝                        │            ║");
+        System.out.println("║                              │                            │            ║");
+        System.out.println("║                              │                          ╔═▼════╗       ║");
+        System.out.println("║                         ╔════▼═╗                        ║YELLOW║       ║");
+        System.out.println("║                         ║VIOLET║                        ╚══════╝       ║");
+        System.out.println("║                         ╚══════╝                                       ║");
         System.out.println("║                                                                        ║");
         System.out.println("╚════════════════════════════════════════════════════════════════════════╝");
         }
